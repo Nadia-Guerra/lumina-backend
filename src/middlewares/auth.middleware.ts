0a7745bccce59dabccbb2a1cab7627004ff04236
Nadia <243741@ids.upchiapas.edu.mx>
2026-04-14 21:54:27 -0600

    initial middleware and configurations (v2 no secrets)

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,19 @@
+import {Request, Response, NextFunction} from 'express';
+import admin from '../config/firebase';
+
+export const verifyToken = async (req:Request, res:Response, next:NextFunction) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(401).json({message: 'No token provided'});
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try{
+        const decoded = await admin.auth().verifyIdToken(token!); //quitar el !
+        (req as any).user = decoded; //correccion rapida, luego se arregla
+        next();
+    }catch(error){
+        return res.status(401).json({message: 'Invalid token'});
+    }  
+}
