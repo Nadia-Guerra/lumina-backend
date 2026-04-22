ca4beb9833a2767b50e3eb182a98778d13a4d873
Emma <reyespreciadoemma@gmail.com>
2026-04-21 23:45:10 -0600

    deleted files

--- src/routes/recommendation.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { getRecommendation } from '../controllers/recommendation.controller.js';
-const router = Router();
-// GET /api/recommendations/:id
-router.get('/:id', getRecommendation);
-export default router;
-//# sourceMappingURL=recommendation.routes.js.map
